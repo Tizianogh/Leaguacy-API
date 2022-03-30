1d7eea7e68cee4df9d49ada9c3ce3397ca1d9c89
Ghisotti <tiziano.ghisotti@gmail.com>
2022-03-30 15:07:19 +0200

    chore(init): initialisation du projet spring

---
+++ src/main/java/fr/dev/leaguacyapi/LeaguacyApplication.java
@@ -0,0 +1,13 @@
+package fr.dev.leaguacyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaguacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaguacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dev/leaguacyapi/LeaguacyApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dev.leaguacyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaguacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
