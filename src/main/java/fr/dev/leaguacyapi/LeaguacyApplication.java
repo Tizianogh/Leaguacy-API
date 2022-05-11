00c770eca7d11c3d5e7b4b7b97e893e42c4af456
Tiziano Ghisotti <tiziaino.ghisotti@gmail.com>
2022-05-11 18:02:46 +0200

    feat(commandlinerunner): separation du commandlinerunner

--- src/main/java/fr/dev/leaguacyapi/LeaguacyApplication.java
+++ src/main/java/fr/dev/leaguacyapi/LeaguacyApplication.java
@@ -1,45 +1,71 @@
 package fr.dev.leaguacyapi;
 
-import org.springframework.boot.SpringApplication;
+import fr.dev.leaguacyapi.domain.model.Player;
+import fr.dev.leaguacyapi.domain.model.Role;
+import fr.dev.leaguacyapi.domain.service.interfaces.LeagueService;
+import fr.dev.leaguacyapi.domain.service.interfaces.PlayerService;
+import fr.dev.leaguacyapi.domain.service.interfaces.RoleService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+import static fr.dev.leaguacyapi.domain.enums.Roles.ADMIN;
+import static fr.dev.leaguacyapi.domain.enums.Roles.JOUEUR;
 
 @SpringBootApplication
 public class LeaguacyApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(LeaguacyApplication.class, args);
-    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-   /* @Bean
-    CommandLineRunner run(PlayerService playerService, RoleService roleService) {
-        return args -> {
-            roleService.createRole(new Role(null, JOUEUR.getValue()));
-            roleService.createRole(new Role(null, ADMIN.getValue()));
-            roleService.createRole(new Role(null, ARBITRE.getValue()));
-
-            playerService.createPlayer(
-                    new Player(null, "Tiziano Ghisotti", "tiziano.ghisotti@gmail.com", "password", new ArrayList<>()));
-            playerService.createPlayer(new Player(null, "Aida Oukrif", "aida.oukrif@gmail.com", "password", new ArrayList<>()));
-            playerService.createPlayer(
-                    new Player(null, "Johan Laforge", "johan.laforge@gmail.com", "password", new ArrayList<>()));
-            playerService.createPlayer(
-                    new Player(null, "Benjamin Krief", "benjamin.krief@gmail.com", "password", new ArrayList<>()));
-            playerService.createPlayer(
-                    new Player(null, "Illiona Bernard", "illiona.bernard@gmail.com", "password", new ArrayList<>()));
-
-            playerService.addRoleToPlayer("tiziano.ghisotti@gmail.com", JOUEUR.toString());
-            playerService.addRoleToPlayer("aida.oukrif@gmail.com", ADMIN.toString());
-            playerService.addRoleToPlayer("johan.laforge@gmail.com", ARBITRE.toString());
-            playerService.addRoleToPlayer("illiona.bernard@gmail.com", ADMIN.toString());
-            playerService.addRoleToPlayer("benjamin.krief@gmail.com", JOUEUR.toString());
-        }; */
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(LeaguacyApplication.class).profiles("dev").run(args);
+    }
 }
 
+@Component
+@Profile(value = "!test")
+class Runner implements CommandLineRunner {
+    private final LeagueService leagueService;
+    private final PlayerService playerService;
+
+    private final RoleService roleService;
+
+    Runner(LeagueService leagueService, PlayerService playerService, RoleService roleService) {
+        this.leagueService = leagueService;
+        this.playerService = playerService;
+        this.roleService = roleService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        roleService.createRole(new Role(null, JOUEUR.getValue()));
+        roleService.createRole(new Role(null, ADMIN.getValue()));
+
+        playerService.createPlayer(
+                new Player(null, "Tiziano Ghisotti", "tiziano.ghisotti@gmail.com", "password", new ArrayList<>()));
+        playerService.createPlayer(new Player(null, "Aida Oukrif", "aida.oukrif@gmail.com", "password", new ArrayList<>()));
+        playerService.createPlayer(
+                new Player(null, "Johan Laforge", "johan.laforge@gmail.com", "password", new ArrayList<>()));
+        playerService.createPlayer(
+                new Player(null, "Benjamin Krief", "benjamin.krief@gmail.com", "password", new ArrayList<>()));
+        playerService.createPlayer(
+                new Player(null, "Illiona Bernard", "illiona.bernard@gmail.com", "password", new ArrayList<>()));
+
+        playerService.addRoleToPlayer("tiziano.ghisotti@gmail.com", JOUEUR.toString());
+        playerService.addRoleToPlayer("aida.oukrif@gmail.com", JOUEUR.toString());
+        playerService.addRoleToPlayer("johan.laforge@gmail.com", JOUEUR.toString());
+        playerService.addRoleToPlayer("illiona.bernard@gmail.com", JOUEUR.toString());
+        playerService.addRoleToPlayer("benjamin.krief@gmail.com", JOUEUR.toString());
+
+    }
+}
--- src/test/java/fr/dev/leaguacyapi/LeagueServiceTest.java
+++ src/test/java/fr/dev/leaguacyapi/LeagueServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.HashMap;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @AutoConfigureMockMvc
 @SpringBootTest
+@ActiveProfiles("test")
 public class LeagueServiceTest {
     @Autowired
     MockMvc mockMvc;
